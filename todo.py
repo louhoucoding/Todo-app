eb9c15ab95276693038336349095da578dfc73d1
= <=>
2024-08-23 13:02:46 +0100

    Rename a task file and remove files separated by ','.

--- todo.py
+++ todo.py
@@ -11,7 +11,6 @@ import pyautogui
 print(datetime.datetime.now())
 print(datetime.datetime.today())
 # List of tasks for the user
-tasks = []
 direction = os.getcwd()
 # Create Direction of Todo
 try:
@@ -19,6 +18,7 @@ try:
 except OSError as e:
     pass
 todo_dir = fr"{direction}\Todo_app"
+print(todo_dir)
 files_list = os.listdir(todo_dir)
 if len(files_list) > 0:
     print("Your Todo list =>", ", ".join(file for file in files_list))
@@ -28,17 +28,20 @@ print(fr"Todo PATH => {direction}\Todo_app")
 
 
 # Function to add a task.
-def add_task(task):
+def add_task():
     """
     Func name: Add_task
     :param: task name
     it adds the task to the tasks list
     :return: name of the task added
     """
+    for i, file in enumerate(files_list, start=1):
+        print(f"{i} => {file}")
+    task = input("Task name: ")
     if task in files_list:
         print("The task file already exists.")
         q2 = input("Do you want to open it?\n$ ").lower()
-        if q2 == "yes":
+        if q2 in ("yes", "y"):
             subprocess.Popen(["notepad.exe", fr"{todo_dir}\{task}"])
             time.sleep(0.5)
             pyautogui.hotkey('ctrl', 'end')
@@ -49,10 +52,6 @@ def add_task(task):
         new_file = fr"{todo_dir}\{task}"
         with open(new_file, "w", encoding="utf-8") as nf:
             nf.write("New file has been created as '" + task + "'.\n")
-
-        tasks.append(task)
-        print(tasks)
-
         q1 = input("Do you want to write in your new task file?\n$ ").lower()
         if q1 == "yes":
             with open(new_file, "a", encoding="utf-8") as file:
@@ -64,7 +63,6 @@ def add_task(task):
             print("Back...")
 
 
-# View the tasks from the list.
 def view_task():
     """
     Func name: View_task
@@ -73,41 +71,68 @@ def view_task():
     :print: the list of tasks if there is a task list
     """
     if len(files_list) > 0:
+        # Display the list of tasks
         for i, file in enumerate(files_list, start=1):
             print(f"[#{i}] => {file}")
-        v_task = input("Task name: ")
+
+        # Asking the user if they want to rename a file
+        rename_file = input("Do you want to rename a file? (yes/y or no/n): \n$ ").strip().lower()
+        if rename_file in ("yes", "y"):
+            task_to_rename = input("Old task name: ").strip()
+
+            # Check if the file exists in the list
+            if task_to_rename in files_list:
+                new_name = input("Task new name: ").strip()
+
+                # Create the full path for old and new task names
+                original_task_name = os.path.join(todo_dir, task_to_rename)
+                new_task_name = os.path.join(todo_dir, new_name)
+                try:
+                    # Renaming the file
+                    os.rename(original_task_name, new_task_name)
+                    print(f"Task renamed from '{task_to_rename}' to '{new_name}'")
+                except FileExistsError:
+                    print(f"A task with the name '{new_name}' already exists. Please choose a different name.")
+                except OSError as E:
+                    print(f"An error occurred while renaming the task: {E}")
+            else:
+                print(f"There is no task with the name '{task_to_rename}'.")
+        elif rename_file in ("no", "n"):
+            print("Back...")
+        else:
+            print("Please check your answer and try again.")
+
+        # Asking the user to view a specific task
+        v_task = input("Task name to view: ").strip()
         if v_task in files_list:
-            with open(fr"{todo_dir}\{v_task}", "r", encoding="utf-8") as f:
+            with open(os.path.join(todo_dir, v_task), "r", encoding="utf-8") as f:
                 print(f.read())
         else:
-            print("There is no task with " + v_task + " name.")
+            print(f"There is no task with the name '{v_task}'.")
     else:
-        print("You files list Is empty.")
+        print("Your files list is empty.")
 
 
+# (The split with ',')
 def remove_task():
     clear = input("Do you want to clear everything?\n$ ").lower()
     if clear in ("yes", "y"):
         for file in files_list:
             os.remove(fr"{todo_dir}\{file}")
     else:
-        ta = " - ".join(files_list)
-        if len(files_list) > 0:
-            print(ta)
-            name_removed_task = input("Task name: ")
-            if name_removed_task in files_list:
-                q1 = (input(f"Do you really want to delete {name_removed_task}?\n$ ").lower())
-                if q1 == "yes":
+        task_to_remove = input("Tasks that you want to remove [Separated by ',']:\n$ ").strip()
+        list_of_tasks_to_remove = task_to_remove.split(",")
+        confirmation = input("Do you really want to delete this tasks file?\n$ ").strip().lower()
+        if confirmation in ("yes", "y"):
+            for lst in list_of_tasks_to_remove:
+                if lst in files_list:
                     try:
-                        os.remove(fr"{todo_dir}\{name_removed_task}")
-                        print(f"{name_removed_task} has been deleted.")
+                        os.remove(fr"{todo_dir}\{lst}")
                     except OSError as E:
                         print("Error =>", E)
-            else:
-                print(f"{name_removed_task}, Is not in the list.")
-
         else:
-            print("Your tasks list is empty.")
+            print("Back...")
+            time.sleep(1)
 
 
 # Competed task.
@@ -121,19 +146,17 @@ def completed_task(num_of_task):
     return num_of_task, "Completed tasks"
 
 
-# List of Options
-print("Choose an option: ")
-print("   [1]=> Add")
-print("   [2]=> View")
-print("   [3]=> Delete")
-print("   [4]=> Completed")
-
-
 # User Interface ==> UI
 while True:
+    # List of Options
+    print("Choose an option: ")
+    print("   [1]=> Add")
+    print("   [2]=> View")
+    print("   [3]=> Delete")
+    print("   [4]=> Completed")
     q = (input("Option: \n$ ").lower())
     if q == "1" or q == "add":
-        add_task(input("Task name: "))
+        add_task()
         break
 
     if q == "2" or q == "view":
@@ -149,8 +172,4 @@ while True:
 
     print("nothing here")
 
-
 # Admin Interface
-
-
-# [1] => Added a function to rename a file I still don't know where
--- try.py
+++ try.py
@@ -1,35 +1,3 @@
-"""
-To-Do list for adding and view your entire tasks list
-with pryvacy and confident
-"""
-import subprocess
-import os
-import datetime
-import time
-print(datetime.datetime.now())
-tasks = []
-direction = os.getcwd()
-print(direction)
-email = "   louhoucode@gmail.com"
-
-def add_task(task):
-    """This is just for pylint"""
-    tasks.append(task)
-    q1 = input("Do you want to write in your new task in a file?\n$ ")
-    while True:
-        if q1 in ("Yes", "yes", "y", "Y"):
-            file_path = fr"{direction}\{task}.txt"
-            with open(file_path, "w") as file:
-                file.write(task)
-            subprocess.run(["notepad.exe", file_path])
-            break
-        if q1 in ("No", "no", "n", "N"):
-            break
-    print(f"{task} has been added Successfully.")
-
-
-while True:
-    q = input("add=>[1]/ view=>[2]/ remove[3]: ")
-    if q == "1":
-        add_task(input("Task name: "))
-        time.sleep(2)
+original_task_name = os.path.join(todo_dir, task_to_rename)
+new_task_name = os.path.join(todo_dir, new_name)
+print(original_task_name, "\n", new_task_name)
